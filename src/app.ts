4f93aae27b4bfc87f9bcfcaf5394129103471ac6
Giovani Coelho <giovanicoelho2004@hotmail.com>
2023-03-28 14:38:52 -0300

    feat: tratamento de erros

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import fastify from 'fastify'
+import { env } from 'process'
+import { ZodError } from 'zod'
 import { appRoutes } from '../src/http/routes'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+// o _ para falar que nao vou usar esse parametro
+app.setErrorHandler((err, _, res) => {
+  if (err instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ message: 'validation error.', issues: err.format })
+  }
+
+  if (env.NODE_ENV !== 'production') console.error(err)
+
+  return res.status(500).send({ message: 'Internal server error.' })
+})
