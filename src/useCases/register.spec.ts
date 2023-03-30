f2724b134578389f0625a3c281157d932bc797f4
Giovani Coelho <giovanicoelho2004@hotmail.com>
2023-03-30 13:31:34 -0300

    test: create user

---
+++ src/useCases/register.spec.ts
@@ -0,0 +1,58 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { compare } from 'bcryptjs'
+import { describe, it, expect } from 'vitest'
+import { UserAlreadyExistsError } from './errors/userAlreadyExistsError'
+import { RegisterUseCase } from './register'
+
+describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      nome: 'Giovani Coelho',
+      email: 'giovanicoelho2004@hotmail.com',
+      password: '12345',
+    })
+
+    // espero que o id do usuario seja do tipo de qualquer string.
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password upan registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      nome: 'Giovani Coelho',
+      email: 'giovanicoelho20034@hotmail.com',
+      password: '12345',
+    })
+
+    const isPasswordCorrectlyHashed = await compare('12345', user.password_hash)
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to regster with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'giovanicoelhoEx@hotmail.com'
+
+    await registerUseCase.execute({
+      nome: 'Giovani Coelho',
+      email,
+      password: '12345',
+    })
+
+    // espero que quando terminar a execucao ela retorne um erro do tipo UserAlreadyExistsError
+    await expect(() =>
+      registerUseCase.execute({
+        nome: 'Giovani Coelho',
+        email,
+        password: '12345',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
--- src/useCases/register.ts
+++ src/useCases/register.ts
@@ -1,6 +1,7 @@
-import { UserAlreadyExistsError } from '../useCases/errors/userAlreadyExistsError'
+import { UserAlreadyExistsError } from '@/useCases/errors/userAlreadyExistsError'
 import { hash } from 'bcryptjs'
-import { IUserRepository } from '../repositories/IUsersRepository'
+import { IUserRepository } from '@/repositories/IUsersRepository'
+import { User } from '@prisma/client'
 
 interface IRegisterUseCase {
   nome: string
@@ -8,11 +9,19 @@ interface IRegisterUseCase {
   password: string
 }
 
+interface IRegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   // eslint-disable-next-line prettier/prettier
   constructor(private usersRepository: IUserRepository) { }
 
-  public async execute({ nome, email, password }: IRegisterUseCase) {
+  public async execute({
+    nome,
+    email,
+    password,
+  }: IRegisterUseCase): Promise<IRegisterUseCaseResponse> {
     // 6 round de codificacao da senha
     // eslint-disable-next-line no-undef
     const passwordHash = await hash(password, 6)
@@ -23,10 +32,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       nome,
       email,
       password_hash: passwordHash,
     })
+
+    return { user }
   }
 }
